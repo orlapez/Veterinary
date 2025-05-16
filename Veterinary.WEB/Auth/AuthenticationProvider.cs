5e077013d171aa0e312c3fa7765ba523cfc274c7
orlapez <orlapez@gmail.com>
2025-05-16 08:01:52 -0500

    Seguridad Web

---
+++ Veterinary.WEB/Auth/AuthenticationProvider.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace VeterinaryWEB.Auth
+{
+    public class AuthenticationProvider : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+
+            var anonimous = new ClaimsIdentity();
+            var oapUser = new ClaimsIdentity(new List<Claim>
+        {
+            new Claim("FirstName", "Luis"),
+            new Claim("LastName", "O"),
+            new Claim(ClaimTypes.Name, "orlapez@gmail.com"),
+                    new Claim(ClaimTypes.Role, "Admin")
+        },
+                authenticationType: "test");
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(oapUser)));
+        }
+    }
+}
--- Veterinary.WEB/Program.cs
+++ Veterinary.WEB/Program.cs
@@ -1,16 +1,21 @@
 using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Veterinary.WEB;
 using Veterinary.WEB.Repositories;
+using VeterinaryWEB.Auth;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7000") });
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7000") });
 
 builder.Services.AddScoped<IRepository, Repository>();
 builder.Services.AddSweetAlert2();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProvider>();
+
 
 await builder.Build().RunAsync();
