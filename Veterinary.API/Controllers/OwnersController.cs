1e668beb9ba4165d7e640c4b56bc0a4f326100e9
orlapez <orlapez@gmail.com>
2026-03-20 08:01:03 -0500

    Controladores

---
+++ Veterinary.API/Controllers/OwnersController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Veterinary.API.Data;
+using Veterinary.Shared.Entities;
+
+namespace Veterinary.API.Controllers
+{
+
+    [ApiController]
+    [Route("/api/owners")]
+    public class OwnersController : Controller
+    {
+        private readonly DataContext _context;
+
+        public OwnersController(DataContext context)
+        {
+            _context = context;
+
+
+        }
+
+
+        // GET: api/owners --Tipo listado de propietarios
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+
+            return Ok(await _context.Owners.ToListAsync());
+
+        }
+
+
+        //Get By Id
+
+        [HttpGet("id:int")]
+
+        public async Task<ActionResult>Get(int id)
+        {
+
+            var owner = await _context.Owners.FirstOrDefaultAsync(x => x.Id==id);
+
+                if (owner == null)
+            {
+                return NotFound();
+
+
+
+            }
+
+            return Ok(owner);
+
+        }
+
+        [HttpPost]
+        public async Task <ActionResult> Post(Owner owner)
+        {
+
+            _context.Add(owner);
+            await _context.SaveChangesAsync();
+            return Ok(owner);
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
